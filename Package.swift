6c55f499b393c95bb3c549c0dbdf7f7d3eb5eba9
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-11-12 18:17:30 +0300

    converted to package

---
+++ Package.swift
@@ -0,0 +1,48 @@
+//swift-tools-version:6.2
+import PackageDescription
+
+let package = Package(
+    name: "ICTMDBHomeModule",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v18)
+    ],
+    products: [
+        .library(
+            name: "ICTMDBHomeModule",
+            targets: ["ICTMDBHomeModule"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.1"),
+        .package(url: "https://github.com/engingulek/GenericCollectionViewKit",from:"0.0.1"),
+        .package(url: "https://github.com/engingulek/ICTMDBModularProtocols", from: "0.0.1"),
+        .package(url: "https://github.com/engingulek/ICTMDBNetworkManagerKit", from: "0.0.1"),
+        .package(url: "https://github.com/engingulek/ICTMDBViewKit", from: "0.0.1"),
+        .package(url: "https://github.com/engingulek/HPDependencyKit", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "ICTMDBHomeModule",
+            dependencies: [
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "GenericCollectionViewKit", package: "GenericCollectionViewKit"),
+                .product(name: "ICTMDBModularProtocols", package: "ICTMDBModularProtocols"),
+                .product(name: "ICTMDBNetworkManagerKit", package: "ICTMDBNetworkManagerKit"),
+                .product(name: "ICTMDBViewKit", package: "ICTMDBViewKit"),
+                .product(name: "DependencyKit", package: "HPDependencyKit")
+            ],
+            path: "ICTMDBHomeModule"
+        ),
+        .testTarget(
+            name: "ICTMDBHomeModuleTests",
+            dependencies: [
+                "ICTMDBHomeModule",
+                .product(name: "GenericCollectionViewKit", package: "GenericCollectionViewKit"),
+                .product(name: "ICTMDBViewKit", package: "ICTMDBViewKit"),
+            ],
+            path: "ICTMDBHomeModuleTests"
+        )
+    ]
+)
+
